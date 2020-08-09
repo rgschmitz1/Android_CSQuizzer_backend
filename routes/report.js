e0eac94eb90d8bf3123c67693a6a26dbe3742b4d
Michael Pham <michaelphamn@yahoo.com>
2020-08-08 21:49:39 -0700

    report question

--- routes/report.js
+++ routes/report.js
@@ -11,7 +11,7 @@ const bodyParser = require("body-parser");
 //This allows parsing of the body of POST requests, that are encoded in JSON
 router.use(bodyParser.json());
 
-router.post("/", (req, res) => {
+router.get("/", (req, res) => {
     res.type("application/json");
     //Retrieve data from query params
     let qid = req.body['qid'];
