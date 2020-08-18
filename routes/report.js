ce5f9390b32132b9816606ea8a628deba795e4c6
Bob Schmitz <14095796+rgschmitz1@users.noreply.github.com>
2020-08-18 12:03:07 -0700

    Updated report to send a copy of message to user

--- routes/report.js
+++ routes/report.js
@@ -1,8 +1,12 @@
 // process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 //express is the framework we're going to use to handle requests
 const express = require('express');
+
 //create a new instance of express router
-var router = express.Router();
+let router = express.Router();
+
+//Create connection to Heroku Database
+let db = require('../utilities/utils').db;
 
 let sendEmail = require('../utilities/utils').sendEmail;
 
@@ -16,12 +20,26 @@ router.post("/", (req, res) => {
     let qid = req.body['qid'];
     let title = req.body['title'];
     let message = req.body['message'];
-    if(qid && message && title) {
-        sendEmail(process.env.PRIMARY_EMAIL, "Report: Question ID: " + qid + ", Question Title: " + title, message);
-        res.send({
-            success: true,
-            message: 'Send report question successful!',
-        });
+    let user = req.body['user'];
+    if(qid && message && title && user) {
+        sendEmail(process.env.PRIMARY_EMAIL,
+            "Report: Question ID: " + qid + ", Question Title: " + title,
+            message);
+        db.one('SELECT Email FROM Members WHERE Username=$1', user)
+            .then(row => {
+                sendEmail(row['email'],
+                    "Copy of Report: Question ID: " + qid + ", Question Title: " + title,
+                    message);
+                res.send({
+                    success: true,
+                    message: 'Send report question successful!',
+                });
+            }).catch((err) => {
+                res.send({
+                    success: false,
+                    error: err
+                });
+            });
     } else {
         res.send({
             success: false,
